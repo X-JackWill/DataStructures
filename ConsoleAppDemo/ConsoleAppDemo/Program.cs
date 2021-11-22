f6af124b455227275b4b606ca2bb5d7f92f1207a
X-Morris <940036850@qq.com>
2021-11-22 09:08:56 +0800

    modified

--- ConsoleAppDemo/ConsoleAppDemo/Program.cs
+++ ConsoleAppDemo/ConsoleAppDemo/Program.cs
@@ -8,6 +8,32 @@ namespace ConsoleAppDemo
         {
             Console.WriteLine("Hello World!");
 
+            /// 找出一些数中的最大数
+
+            // 定义一个数组，存储一些数
+            int[] nums = { 2, 4, 6, 9, 20, 21 , 3, 5};
+            // 默认数组的第一个元素为最大值
+            int Max = nums[0];
+            // 通过for循环，当前数与最大的数进行对比
+            for (int i = 0; i < nums.Length; i++) {
+                int num = nums[i];
+                // 如果当前的数大于最大的数，将当前的数作为做大数
+                if (Max < num) {
+                    Max = num;
+                }
+                // 当所有的数都比较完之后，输出最大数
+                if (i >= nums.Length - 1) {
+                    Console.WriteLine(Max);
+                }
+            }
+            
+
+
+
+
+
+
+
 
             ///// 自定义数组
             //MMIntArray intArray = new MMIntArray();
@@ -49,16 +75,16 @@ namespace ConsoleAppDemo
             //Console.WriteLine(sulution.IsValid(str1));
 
 
-            // 队列
-            MMArrayQueue<object> arrayQueue = new MMArrayQueue<object>();
-            arrayQueue.Enqueue(1);
-            arrayQueue.Enqueue(2);
-            arrayQueue.Enqueue(3);
-            Console.WriteLine(arrayQueue.GetFront());
-            arrayQueue.Dequeue();
-            arrayQueue.Dequeue();
-            arrayQueue.Dequeue();
-            Console.WriteLine(arrayQueue.GetFront());
+            //// 队列
+            //MMArrayQueue<object> arrayQueue = new MMArrayQueue<object>();
+            //arrayQueue.Enqueue(1);
+            //arrayQueue.Enqueue(2);
+            //arrayQueue.Enqueue(3);
+            //Console.WriteLine(arrayQueue.GetFront());
+            //arrayQueue.Dequeue();
+            //arrayQueue.Dequeue();
+            //arrayQueue.Dequeue();
+            //Console.WriteLine(arrayQueue.GetFront());
         }
     }
 
